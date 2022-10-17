c26fc279f351a2a373ea2afaec02cc01b55b59b8
Rey  Baterbonia <poochinng@gmail.com>
2022-10-17 22:00:49 +0800

    init

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 var character = document.getElementById('character');
-var blocks = document.getElementById('blocks');
+var block = document.getElementById('blocks');
 
-function jump(){
+function jump (){
+
     if(character.classList != 'animate'){
         character.classList.add('animate');
     }
     setTimeout(function(){
-        character.classList.remove('animate')
+        character.classList.remove('animate');
     }, 500)
 }
 
 var checkDead = setInterval(function(){
 
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue('top'));
-    var blockLeft = parseInt(window.getComputedStyle(blocks).getPropertyValue('left'));
+    blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue('left'));
 
-    if(blockLeft < 40 && blockLeft > 0 && characterTop>= 130){
-        blocks.style.animation = 'none';
-        blocks.style.display = 'none';
-        alert('You Lose!');
+    if(blockLeft < 39 && blockLeft > 0 && characterTop>= 130){
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        alert('You Lose')
     }
-}, 10)
+},10)
